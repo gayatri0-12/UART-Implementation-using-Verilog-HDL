f65d8ee3d92b06da92ba1c6eef8afb04f92e7227
gayatri0-12 <shinyy8065@gmail.com>
2026-03-26 16:47:06 +0530

    Update SipoTest.v

--- UART-Implementation-using-Verilog-HDL-main/Code/UART-Rx/SipoTest.v
+++ UART-Implementation-using-Verilog-HDL-main/Code/UART-Rx/SipoTest.v
@@ -1,6 +1,3 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
 //  FILE NAME: SipoTest.v
 //  TYPE: Test fixture "Test bench".
 //  DATE: 31/8/2022
